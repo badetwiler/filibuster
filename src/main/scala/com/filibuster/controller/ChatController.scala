5960957de572d5f56a5fa1685837467fe8c690e8
Ben Detwiler <bendetwiler@gmail.com>
2014-01-04 12:20:39 +0000

    Add web service dependencies to pom.xml

--- src/main/scala/com/filibuster/controller/ChatController.scala
+++ src/main/scala/com/filibuster/controller/ChatController.scala
@@ -2,7 +2,7 @@ package com.filibuster.controller
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.{RequestParam, ResponseBody, RequestMapping, RequestMethod}
-
+import com.gusto.filibuster.client._
 
 /**
  *
