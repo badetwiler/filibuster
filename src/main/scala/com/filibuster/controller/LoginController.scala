46dbb9980f98040aafa4392ef732ad872e1a2f47
Ben Detwiler <bendetwiler@gmail.com>
2014-01-24 22:59:00 +0000

    Add creatUser to LoginController

--- src/main/scala/com/filibuster/controller/LoginController.scala
+++ src/main/scala/com/filibuster/controller/LoginController.scala
@@ -2,15 +2,29 @@ package com.filibuster.controller
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.{RequestParam, ResponseBody, RequestMapping, RequestMethod}
-
-
+import org.springframework.beans.factory.annotation.Autowired
+import com.filibuster.data.FilibusterDataManager
+import com.filibuster.model.User
 
 @Controller
-class LoginController {
+class LoginController @Autowired() (dataManager:FilibusterDataManager)
+{
 
   @RequestMapping(value = Array("/login"), method = Array(RequestMethod.GET))
   def login():String = {
     "login"
+
+  }
+
+  @RequestMapping(value = Array("/createUser"), method = Array(RequestMethod.GET))
+  def createUser(@RequestParam("username") username:String,
+                 @RequestParam("password") password:String,
+                 @RequestParam("email")    email:String     ) =
+  {
+    val salt = ""
+
+    val user = User(username,1,email,salt,password) //need to hash pw first
+    dataManager.insertUser(user)
 
   }
 
